f8f8cf13f0a8a34828cb49b0b4fcf088508c5124
Subinkhang <subinkhang102@gmail.com>
2023-12-31 00:32:26 +0700

    add logic of change callculator + add unit jest test + fix UI

---
+++ client/index.ts
@@ -0,0 +1,12 @@
+// src/index.ts
+
+describe("My Test Suite", () => {
+  it("should do something", () => {
+    // ...
+  });
+  
+  it("should do something else", () => {
+    // ...
+  });
+});
+  
